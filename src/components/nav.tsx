0451f94f50a5eebc674326dee35c663212c595df
H-Blues <hblues419@gmail.com>
2024-07-21 22:35:07 +0100

    style: update nav icons with the library

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
+import { Home, Award, ClipboardList, Users } from "lucide-react";
 
 const Nav: React.FC = () => {
   const location = useLocation();
@@ -8,60 +9,44 @@ const Nav: React.FC = () => {
     {
       path: "/",
       label: "Home",
-      icon: (
-        <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-          <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
-        </svg>
-      ),
+      icon: <Home size={24} />,
     },
     {
       path: "/leaderboard",
       label: "Leaderboard",
-      icon: (
-        <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-          <path d="M5 4a2 2 0 012-2h6a2 2 0 012 2v14l-5-2.5L5 18V4z" />
-        </svg>
-      ),
+      icon: <Award size={24} />,
     },
     {
       path: "/quest",
       label: "Quest",
-      icon: (
-        <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-          <path
-            fillRule="evenodd"
-            d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v2H7a1 1 0 100 2h2v2a1 1 0 102 0v-2h2a1 1 0 100-2h-2V7z"
-            clipRule="evenodd"
-          />
-        </svg>
-      ),
+      icon: <ClipboardList size={24} />,
     },
     {
       path: "/friends",
       label: "Friends",
-      icon: (
-        <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-          <path d="M9 6a3 3 0 11-6 0 3 3 0 016 0zM17 6a3 3 0 11-6 0 3 3 0 016 0zM12.93 17c.046-.327.07-.66.07-1a6.97 6.97 0 00-1.5-4.33A5 5 0 0119 16v1h-6.07zM6 11a5 5 0 015 5v1H1v-1a5 5 0 015-5z" />
-        </svg>
-      ),
+      icon: <Users size={24} />,
     },
   ];
 
   return (
-    <nav className="fixed bottom-0 left-0 right-0 bg-white border-t border-gray-200">
-      <div className="flex justify-around items-center h-16">
-        {navItems.map((item) => (
-          <Link
-            key={item.path}
-            to={item.path}
-            className={`flex flex-col items-center ${location.pathname === item.path ? "text-black" : "text-gray-400"}`}
-          >
-            {item.icon}
-            <span className="mt-1 text-xs">{item.label}</span>
-          </Link>
-        ))}
-      </div>
-    </nav>
+    <div>
+      <nav className="fixed bottom-0 left-0 right-0 bg-white border-t border-gray-200">
+        <div className="flex justify-around items-center h-16">
+          {navItems.map((item) => (
+            <Link
+              key={item.path}
+              to={item.path}
+              className={`flex flex-col items-center ${
+                location.pathname === item.path ? "text-black" : "text-gray-400"
+              }`}
+            >
+              {item.icon}
+              <span className="mt-1 text-xs">{item.label}</span>
+            </Link>
+          ))}
+        </div>
+      </nav>
+    </div>
   );
 };
 
