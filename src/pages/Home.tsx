4652d9593bd4bd92c5f816838bfb51ffde46c9ff
H-Blues <hblues419@gmail.com>
2024-07-25 00:57:29 +0100

    test: send message to bot

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import Nav from "../components/nav";
 import honeyLogo from "../assets/honey.svg";
 import HexagonRow from "../components/hexagonRow";
 // import RegisterTime from "../components/registerTime";
 
+import WebApp from "@twa-dev/sdk";
+
 const Home: React.FC = () => {
+  const [msg, setMsg] = useState("Hi");
+
+  const join = () => {
+    setMsg(JSON.stringify(WebApp.initDataUnsafe.user?.id));
+    WebApp.sendData(JSON.stringify(WebApp.initDataUnsafe.user?.id));
+  };
+
   return (
     <div className="flex flex-col min-h-screen w-full bg-white">
       <div className="w-full relative mt-6">
@@ -27,7 +36,10 @@ const Home: React.FC = () => {
         <div className="bg-gray-100 rounded-lg p-4 w-full max-w-sm mx-auto text-center">
           <h2 className="text-xl font-bold">HONEY COMMUNITY</h2>
           <p className="text-yellow-500 mt-4">Home for telegram OGs</p>
-          <button className="bg-yellow-400 text-black font-bold py-2 px-8 rounded-full mt-4 w-full">Join</button>
+          <p className="text-black">{msg}</p>
+          <button className="bg-yellow-400 text-black font-bold py-2 px-8 rounded-full mt-4 w-full" onClick={join}>
+            Join
+          </button>
         </div>
       </div>
 
